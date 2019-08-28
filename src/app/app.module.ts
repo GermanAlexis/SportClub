bfd8ad863156dd8a647c5cadf193d3af0cd44fce
Alexis <alexisdager93@hotmai.com>
2019-08-28 11:42:50 -0500

    up_ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { BodyComponent } from './Components/body/body.component';
+import { BannerComponent } from './Components/banner/banner.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    BannerComponent,
+
   ],
   imports: [
     BrowserModule,
